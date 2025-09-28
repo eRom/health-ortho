7f1ba1bd38f4f8015f1b4de71d6bb0e29beee4ec
eRom <romain.ecarnot@gmail.com>
2025-09-28 12:59:08 +0200

    Update index.html to correct the platform link path and remove neuro.html as part of project restructuring. Enhance VSCode settings with custom color themes for improved development experience.

---
+++ neuro/exercices_neuro.js
@@ -0,0 +1,12 @@
+export const neuroExercises = [
+  {
+    id: "empans",
+    name: "Empans ",
+    description:
+      "Entraînez la capacité de mémorisation grâce à des séries de lettres ou de chiffres.",
+    href: "empans/",
+    tags: ["mémoire de travail", "attention"],
+    level: "Tous niveaux",
+  },
+];
+
