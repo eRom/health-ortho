8c8b7c56dc5ec34803fd8b7c664337e4213550c2
eRom <romain.ecarnot@gmail.com>
2025-09-29 19:24:39 +0200

    Remove remerciements.html file and update neuroExercises to reflect changes in tags for improved clarity.

--- neuro/exercices_neuro.js
+++ neuro/exercices_neuro.js
@@ -5,7 +5,7 @@ export const neuroExercises = [
     description:
       "Entraînez la capacité de mémorisation grâce à des séries de lettres ou de chiffres.",
     href: "empans/",
-    tags: ["mémoire de travail", "attention"],
+    tags: ["mémoire de travail"],
     level: "Tous niveaux",
   },
   {
